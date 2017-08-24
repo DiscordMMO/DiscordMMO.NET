410a908002219d8c9314443f39b5fa82534f3588
TheMagzuz <markus.dam123@gmail.com>
2017-08-24 18:23:19 +0200

    Moved the modules class out of datatypes

--- DiscordMMO/Modules.cs
+++ DiscordMMO/Modules.cs
@@ -12,7 +12,7 @@ using DiscordMMO.Datatypes.Inventories;
 using DiscordMMO.Helpers;
 using Action = DiscordMMO.Datatypes.Actions.Action;
 
-namespace DiscordMMO.Datatypes
+namespace DiscordMMO
 {
     public static class Modules
     {
