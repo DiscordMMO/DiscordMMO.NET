e92e0b101f3de89df650816504e7579b9186744f
TheMagzuz <markus.dam123@gmail.com>
2017-09-19 22:01:59 +0200

    Added more comments

--- DiscordMMO/Modules.cs
+++ DiscordMMO/Modules.cs
@@ -4,7 +4,9 @@ using DiscordMMO.Consts;
 using DiscordMMO.Datatypes.Actions;
 using DiscordMMO.Datatypes.Preferences;
 using DiscordMMO.Handlers;
+using ProtoBuf;
 using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
@@ -52,28 +54,27 @@ namespace DiscordMMO
     public class NoPrefix : ModuleBase
     {
 
-
-        [Command("ping"), Summary("Pong")]
-        public async Task Ping()
-        {
-            await ReplyAsync("Pong!");
-        }
-
         [Command("register"), Summary("Register with the name, if one is given")]
         public async Task Register([Summary("The name to register with")] string name = null)
         {
+            // Check if no name parameter was given
             if (name == null || name.Trim().Equals(""))
             {
+                // Set the players username to the discord username of the sender
                 name = Context.User.Username;
             }
+            // Check if the player already has a user
             if (await PlayerHandler.AttemptLogin(Context.User))
             {
+                // Notify the player that they already have player
                 await ReplyAsync($"{Context.User.Username}: {Modules.ALREADY_REGISTERED_MSG}");
                 return;
             }
             else
             {
+                // Create the player instance
                 PlayerHandler.CreatePlayer(Context.User, name);
+                // Notify the player that they have been registered
                 await ReplyAsync($"{Context.User.Username}: {String.Format(Modules.REGISTERED_FORMAT, name)}");
             }
 
@@ -82,63 +83,84 @@ namespace DiscordMMO
         [Command("status"), Summary("Gets what the optional user is doing")]
         public async Task Status(IUser user = null)
         {
-            if (user == null)
+
+            bool firstPerson = user == null;
+
+            // Check if a user was given
+            if (firstPerson)
             {
-                if (!await PlayerHandler.AttemptLogin(Context.User))
-                {
-                    await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
-                    return;
-                }
+                // If no user was given, use the sender
                 user = Context.User;
-                Player player = PlayerHandler.GetPlayer(user);
-                string doneIn = String.Format(Action.DONE_IN_FORMAT, ((DateTime)player.currentAction.finishTime - DateTime.Now));
-                await ReplyAsync(Context.User.Username + ": " + player.currentAction.GetActiveFormattingSecondPerson() +
-                    ((!player.currentAction.setFinishTime) ? "" : doneIn));
-                return;
             }
+
+            // Check if the player has a user
             if (!await PlayerHandler.AttemptLogin(user))
             {
-                await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_THIRD_PERSON(user.Username));
+                // If the player is not registered, notify them
+                await ReplyAsync(Context.User.Username + ": " + (firstPerson ? Modules.NOT_REGISTERED_MSG
+                    : Modules.NOT_REGISTERED_THIRD_PERSON(user.Username)));
                 return;
             }
-            Player target = PlayerHandler.GetPlayer(user);
-            await ReplyAsync(Context.User.Username + ": " + target.currentAction.GetActiveFormattingThridPerson(false));
+            Player player = PlayerHandler.GetPlayer(user);
+
+            // Format the done in
+            string doneIn = (firstPerson ? " You" : " They") + String.Format(Action.DONE_IN_FORMAT, (player.currentAction.finishTime - DateTime.Now));
+
+            // Message the player with the status
+            await ReplyAsync(Context.User.Username + ": " + 
+                (firstPerson ? player.currentAction.GetActiveFormattingSecondPerson() : player.currentAction.GetActiveFormattingThridPerson(false)) +
+                ((!player.currentAction.hasSetFinishTime) ? "" : doneIn));
+            return;
 
         }
 
         [Command("chop"), Summary("Starts chopping wood")]
         public async Task Chop()
         {
+            // Check if the player can log in
             if (!await PlayerHandler.AttemptLogin(Context.User))
             {
+                // If they cannot login, notify them
                 await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
                 return;
             }
 
             Player p = PlayerHandler.GetPlayer(Context.User);
 
+            // Check if the player is already doing something
             if (!p.IsIdle)
             {
+                // Notify the player if they are already doing something
                 await ReplyAsync(Context.User.Username + ": " + String.Format(Modules.ALREADY_ACTIVE_TIME_LEFT_FORMAT, p.currentAction.finishTime - DateTime.Now));
                 return;
             }
 
+            // Set the players action
             p.SetAction(new ActionChopWood(p), p.GetPreference<bool>("pm"), false);
+
             Action a = p.currentAction;
+            // Notify the player
             await ReplyAsync(Context.User.Username + ": " + a.GetStartedFormattingSecondPerson());
         }
 
         [Command("pref"), Summary("View or edit preferences")]
         public async Task Prefs(string prefName = null, string value = null)
         {
+
+            // Check if the player can log in
             if (!await PlayerHandler.AttemptLogin(Context.User))
             {
+                // If they cannot login, notify them
                 await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
                 return;
             }
+
             Player p = PlayerHandler.GetPlayer(Context.User);
+
+            // Check if neither a preference name or a value was given
             if (String.IsNullOrWhiteSpace(value) && String.IsNullOrWhiteSpace(prefName))
             {
+                // Loop through all the preferences and show their value
                 StringBuilder outp = new StringBuilder(Modules.PREF_MSG_START);
                 foreach (string key in p.GetPreferences().Keys)
                 {
@@ -148,25 +170,47 @@ namespace DiscordMMO
                 await ReplyAsync(outp.ToString());
                 return;
             }
+            // Check if only a preference name was given
             else if (!String.IsNullOrWhiteSpace(prefName) && String.IsNullOrWhiteSpace(value))
             {
                 IPreference pref = p.GetPreferences()[prefName];
 
+                // Check if the preference does not exist
                 if (pref == null)
                 {
                     await ReplyAsync(Context.User.Username + ": That preference does not exist");
                     return;
                 }
 
+                // Tell the player the value of the preference
                 await ReplyAsync(Context.User.Username + ": " + prefName + " is " + pref);
                 return;
             }
+            // Check if both a preference name and value was given
             else if (!String.IsNullOrWhiteSpace(prefName) && !String.IsNullOrWhiteSpace(value))
             {
                 IPreference pref = p.GetPreferences()[prefName];
+
+                // Check if the preference does not exist
+                if (pref == null)
+                {
+                    await ReplyAsync(Context.User.Username + ": That preference does not exist");
+                    return;
+                }
+
+
+                // Get the type of the preference
                 Type t = pref.type;
+
+                // Create the value object
                 var toSet = Convert.ChangeType(value, t);
+
+                // Set the value of the preference
                 p.SetPreferenceWithType(prefName, toSet, t);
+
+                // Notify the player that the preference was changed
+                await ReplyAsync($"{Context.User.Username}: Set preference {prefName} to {value}");
+
             }
 
         }
@@ -174,12 +218,17 @@ namespace DiscordMMO
         [Command("login")]
         public async Task LoginCommand()
         {
+            // Check if the player can log in
             if (!await PlayerHandler.AttemptLogin(Context.User))
             {
+                // If they cannot login, notify them
                 await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
                 return;
             }
+
             Player p = PlayerHandler.GetPlayer(Context.User);
+
+            // Notify the player that they have been logged in
             await ReplyAsync(Context.User.Username + ": " + Modules.LOGGED_IN_AS + p.playerName);
         }
 
@@ -187,23 +236,32 @@ namespace DiscordMMO
         public async Task InventoryCommand()
         {
 
-
+            // Check if the player can log in
             if (!await PlayerHandler.AttemptLogin(Context.User))
             {
+                // If they cannot login, notify them
                 await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
                 return;
             }
 
             Player player = PlayerHandler.GetPlayer(Context.User);
+
+            // Create the string builder for the message
             StringBuilder outp = new StringBuilder($"Inventory for {player.playerName}\n");
+
+            // Append the free spaces to the message
             outp.Append($"{player.inventory.FreeSpaces}/{player.inventory.size} Slots available\n");
+
+            // Loop through all the items
             int i = 0;
             foreach (ItemStack stack in player.inventory.items)
             {
+                // Change to a new line every 5 items
                 if (i % 5 == 0)
                 {
                     outp.Append("\n");
                 }
+                // If the stack is null, treat it as empty
                 if (stack == null)
                 {
                     outp.Append(ItemStack.empty.itemType.displayName);
@@ -221,28 +279,36 @@ namespace DiscordMMO
         [Command("equip"), Summary("Equip an item")]
         public async Task EquipItemCommand(int itemToEqiupIndex)
         {
+            // Check if the player can log in
             if (!await PlayerHandler.AttemptLogin(Context.User))
             {
+                // If they cannot login, notify them
                 await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
                 return;
             }
 
             Player p = PlayerHandler.GetPlayer(Context.User);
 
-            ItemStack itemToEquip = p.inventory[itemToEqiupIndex+1];
+            // Get the item to equip
+            // The +1 is to have slots 1-28, rather than 0-27
+            ItemStack itemToEquip = p.inventory[itemToEqiupIndex + 1];
 
-            if (itemToEquip.IsEmpty)
+            // Check if there is an item in the given slot
+            if (itemToEquip == null || itemToEquip.IsEmpty)
             {
                 await ReplyAsync(p.playerName + ": That slot is empty");
                 return;
             }
+            // Check if the item is equipable
             if (itemToEquip.itemType is ItemEquipable == false)
             {
                 await ReplyAsync(p.playerName + ": You cannot equip that item");
                 return;
             }
+            // Equip the item
             p.Equip(itemToEquip);
 
+            // Notify the user that the item was equipped
             await ReplyAsync(p.playerName + ": Equipped " + itemToEquip.itemType.displayName);
 
         }
@@ -250,18 +316,27 @@ namespace DiscordMMO
         [Command("equipment")]
         public async Task EquipmentCommand()
         {
+            // Check if the player can log in
             if (!await PlayerHandler.AttemptLogin(Context.User))
             {
+                // If they cannot login, notify them
                 await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
                 return;
             }
 
             Player player = PlayerHandler.GetPlayer(Context.User);
+
+            // Create a stringbuilder for the output
             StringBuilder outp = new StringBuilder($"Equipment for {player.playerName}\n");
+            
+            // Loop through the players equipment
             int i = 0;
             foreach (ItemStack stack in player.equipment.items)
             {
+                // Add the name of the slot to the string
                 outp.Append(((PlayerEquipmentSlot)i).GetDisplayName() + ": ");
+
+                // If the slot is null, treat the item as empty
                 if (stack == null)
                 {
                     outp.Append(ItemStack.empty.itemType.displayName);
@@ -270,25 +345,33 @@ namespace DiscordMMO
                 {
                     outp.Append(stack.ToStringNoCount() + " ");
                 }
+
+                // Change to a new line
                 outp.Append("\n");
+
                 i++;
             }
+            // Message the playe
             await ReplyAsync(outp.ToString());
         }
 
         [Command("fight")]
         public async Task FightCommand()
         {
+            // Check if the player can log in
             if (!await PlayerHandler.AttemptLogin(Context.User))
             {
+                // If they cannot login, notify them
                 await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
                 return;
             }
 
             Player player = PlayerHandler.GetPlayer(Context.User);
 
+            // Start a fight against a new EntityGoblin
             player.StartFight(new EntityGoblin(), true);
 
+            // Notify the player
             await ReplyAsync("Started fighting");
 
         }
@@ -311,27 +394,38 @@ namespace DiscordMMO
         [Command("save")]
         public async Task SaveCommand()
         {
+            // Save everything
             await DatabaseHandler.SaveAllAsync();
         }
 
         [Command("players")]
         public async Task ListPlayers()
         {
+            // Create a stringbuilder for all players
             StringBuilder outp = new StringBuilder("Players: \n");
 
+            // Loop through all players
             foreach (Player p in PlayerHandler.GetPlayers())
             {
+                // Append the player and their current action to the stringbuilder
                 outp.Append(p.playerName + ": " + p.currentAction + "\n");
             }
 
+            // Output the stringbuilder
             await ReplyAsync(outp.ToString());
         }
 
         [Command("reply")]
         public async Task Reply()
         {
-            if (! await PlayerHandler.AttemptLogin(Context.User))
+            // Check if the player can log in
+            if (!await PlayerHandler.AttemptLogin(Context.User))
+            {
+                // If they cannot login, notify them
+                await ReplyAsync(Context.User.Username + ": " + Modules.NOT_REGISTERED_MSG);
                 return;
+            }
+
             Player p = PlayerHandler.GetPlayer(Context.User);
             string eq = p.equipment.ToString();
             await ReplyAsync(p.equipment.ToString());
@@ -388,16 +482,28 @@ namespace DiscordMMO
         [Command("ser")]
         public async Task XmlCommand()
         {
+
+            //FATAL: Fix the stackoverflow 
+
             int count = 100;
             await ReplyAsync("Serializing " + count + " ItemStacks");
             Stopwatch w = Stopwatch.StartNew();
             for (int i = 0; i < count; i++)
             {
-                byte[] s = SerializationHandler.Serialize((ItemStack)ItemHandler.GetItemInstanceFromName("wood"));
+                byte[] s;
+                using (MemoryStream mem = new MemoryStream())
+                {
+                    Serializer.NonGeneric.Serialize(mem, (ItemStack)ItemHandler.GetItemInstanceFromName("wood"));
+                    s = mem.ToArray();
+                }
+                using (MemoryStream mem = new MemoryStream(s))
+                {
+                    ItemStack item = Serializer.Deserialize<ItemStack>(mem);
+                }
             }
             w.Stop();
             await ReplyAsync("Serializing " + count + " objects took " + w.ElapsedMilliseconds + "ms \n" +
-                             "Average time per object: " + w.ElapsedMilliseconds/count  + "ms");
+                             "Average time per object: " + w.ElapsedMilliseconds / count + "ms");
 
         }
 
