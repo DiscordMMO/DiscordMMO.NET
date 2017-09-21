bc620df79e2473799809e9fc46066d4e99ab9fc5
TheMagzuz <markus.dam123@gmail.com>
2017-09-21 09:08:31 +0200

    Add comments

--- DiscordMMO/Server.cs
+++ DiscordMMO/Server.cs
@@ -15,6 +15,7 @@ namespace DiscordMMO
 
         private volatile bool stop = false;
 
+        // The amount of ticks per second
         private static double tickRate = 2;
 
         public static Server INSTANCE { get; private set; }
@@ -24,24 +25,33 @@ namespace DiscordMMO
 
         static Server()
         {
+            #region Singleton
             if (INSTANCE == null)
                 INSTANCE = new Server();
+            #endregion
         }
 
         public async void Run()
         {
             while (!stop)
             {
+                // Tick every player
                 List<Task> playerTicks = new List<Task>();
                 foreach (Player p in PlayerHandler.GetPlayers())
                 {
                     playerTicks.Add(p.Tick());
                 }
+                // Make sure every player has been ticked
                 await Task.WhenAll(playerTicks);
+                
+                // Wait for the next tick to occur
                 await Task.Delay((int)(1000 / tickRate));
                 ticksSinceLastSave++;
+                
+                // Save every tickBetweenSaves ticks
                 if (ticksSinceLastSave >= tickBetweenSaves && Program.sqlAvailable)
                 {
+                    // Save everything
                     DatabaseHandler.SaveAllAsync();
                     ticksSinceLastSave = 0;
                 }
