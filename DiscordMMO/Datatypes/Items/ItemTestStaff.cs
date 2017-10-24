0de009f3b6e342684cdea9e40fca655623777b05
TheMagzuz <markus.dam123@gmail.com>
2017-10-24 21:45:04 +0200

    Made a test magic weapon

---
+++ DiscordMMO/Datatypes/Items/ItemTestStaff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordMMO.Datatypes.Items
+{
+    public class ItemTestStaff : ItemMagicWeapon
+    {
+        public override int manaUsage => 60;
+
+        public override int attackRate => 5;
+
+        public override int attackDamage => 5;
+
+        public override int accuracy => 75;
+
+        public override string itemName => "test_staff";
+
+        public override string displayName => "Test Staff";
+    }
+}
