3ff08d4113eb671eca068683a33abe9c57b72f6e
TheMagzuz <markus.dam123@gmail.com>
2017-10-29 20:54:29 +0100

    Add consumables

---
+++ DiscordMMO/Datatypes/Items/Consumable/ItemConsumable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordMMO.Datatypes.Items.Consumable
+{
+    public abstract class ItemConsumable : Item
+    {
+        public abstract void Consume(Player player);
+    }
+}
---
+++ DiscordMMO/Datatypes/Items/Consumable/ItemHealingPotion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordMMO.Datatypes.Entities;
+
+namespace DiscordMMO.Datatypes.Items.Consumable
+{
+    public class ItemHealingPotion : ItemConsumable
+    {
+
+        public readonly int healAmount = 10;
+
+        public override string itemName => "healing_potion";
+
+        public override string displayName => "Healing Potion";
+
+        public override bool stackable => false;
+
+        public override void Consume(Player player)
+        {
+            player.Heal(healAmount);
+        }
+    }
+}
