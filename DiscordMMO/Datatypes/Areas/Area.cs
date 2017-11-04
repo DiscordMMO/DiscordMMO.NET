f0fb6f93ab59ec4aef8381f18ed9d2eb979bada0
TheMagzuz <markus.dam123@gmail.com>
2017-11-04 10:23:59 +0100

    More work on areas

--- DiscordMMO/Datatypes/Areas/Area.cs
+++ DiscordMMO/Datatypes/Areas/Area.cs
@@ -17,5 +17,15 @@ namespace DiscordMMO.Datatypes.Areas
 
         public List<Entity> content { get; protected set; }
 
+        /// <summary>
+        ///  Get the time it takes in seconds to move to this area via the direction <paramref name="from"/>
+        /// </summary>
+        /// <param name="from">The direction to move from</param>
+        /// <returns></returns>
+        public virtual int GetMoveTime(Direction from)
+        {
+            return 600;
+        }
+
     }
 }
---
+++ DiscordMMO/Handlers/AreaHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordMMO.Datatypes.Areas;
+using System.Drawing;
+
+namespace DiscordMMO.Handlers
+{
+    public static class AreaHandler
+    {
+
+        static Dictionary<Point, Area> map = new Dictionary<Point, Area>();
+
+        public static void LoadArea(int x, int y, Area tile, bool forced = false, bool silent = false)
+        {
+            if (map.ContainsKey(new Point(x,y)))
+            {
+                if (!silent)
+                {
+                    Console.Write($"Tried to load an area at an alredy loaded position at: ({x},{y})");
+                    Console.WriteLine("The area was " + (forced ? "" : "not ") + "loaded");
+                }
+                if (!forced)
+                    return;
+            }
+
+            map[new Point(x, y)] = tile;
+
+        }
+
+        public static Area GetArea(int x, int y)
+        {
+            return map[new Point(x, y)];
+        }
+
+
+    }
+}
