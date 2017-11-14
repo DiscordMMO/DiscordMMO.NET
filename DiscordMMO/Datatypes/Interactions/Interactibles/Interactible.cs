b34edc753531c761307f3a26df5c198102954fce
TheMagzuz <markus.dam123@gmail.com>
2017-11-14 20:57:01 +0100

    Made interactibles work with serialization

--- DiscordMMO/Datatypes/Interactions/Interactibles/Interactible.cs
+++ DiscordMMO/Datatypes/Interactions/Interactibles/Interactible.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
+using System.Xml.Serialization;
 
 namespace DiscordMMO.Datatypes.Interactions.Interactibles
 {
+    [XmlRoot]
     public abstract class Interactible
     {
 
+        [XmlElement]
         public abstract string name { get; set; }
+
+        [XmlIgnore]
         public abstract string displayName { get; set; }
 
-
+        [XmlElement]
         public virtual List<Interaction> interactions { get; set; }
 
         public Interactible()
