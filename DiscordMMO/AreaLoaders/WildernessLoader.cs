15f278a6d1d7c51a3de292fc32ea304626d98e6e
TheMagzuz <markus.dam123@gmail.com>
2017-11-14 20:32:41 +0100

    Added a wilderness loader

---
+++ DiscordMMO/AreaLoaders/WildernessLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordMMO.Handlers;
+using DiscordMMO.Datatypes.Areas;
+using DiscordMMO.Factories;
+using DiscordMMO.Datatypes.Entities;
+
+namespace DiscordMMO.AreaLoaders
+{
+    public class WildernessLoader : IAreaLoader
+    {
+        public void Load()
+        {
+            AreaWilderness area = new AreaWilderness();
+            area.content.Add(EntityFactory.CreateFightable<EntityGoblin>());
+            AreaHandler.LoadArea(0, 1, area);
+        }
+
+        public void PostLoad()
+        {
+        }
+
+        public void PreLoad()
+        {
+        }
+    }
+}
