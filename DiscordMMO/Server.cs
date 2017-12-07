d4b6eeb176fab50535b00ace480ba2359c193bd4
TheMagzuz <markus.dam123@gmail.com>
2017-12-07 21:54:18 +0100

    Supress a warning

--- DiscordMMO/Server.cs
+++ DiscordMMO/Server.cs
@@ -52,7 +52,10 @@ namespace DiscordMMO
                 if (ticksSinceLastSave >= tickBetweenSaves && Program.sqlAvailable)
                 {
                     // Save everything
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     DatabaseHandler.SaveAllAsync();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     ticksSinceLastSave = 0;
                 }
 
