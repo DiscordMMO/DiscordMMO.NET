7778bee0fb942d4602760650e9eca3cd9aa7a564
TheMagzuz <markus.dam123@gmail.com>
2017-12-07 21:42:58 +0100

    Supressed some warnings

--- DiscordMMO/Datatypes/Entities/IDamageable.cs
+++ DiscordMMO/Datatypes/Entities/IDamageable.cs
@@ -224,6 +224,7 @@ namespace DiscordMMO.Datatypes.Entities
 
             if (target.health <= 0)
             {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 target.Die(attacker);
                 return true;
             }
