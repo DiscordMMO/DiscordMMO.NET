b0b7d0a2bb03ab9456c5ca7d4a7c17f69619e997
TheMagzuz <markus.dam123@gmail.com>
2017-12-07 20:46:44 +0100

    Add an interaction to display loot

---
+++ DiscordMMO/Datatypes/Interactions/InteractionLookLoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using DiscordMMO.Util;
+
+namespace DiscordMMO.Datatypes.Interactions
+{
+    public class InteractionLookLoot : Interaction
+    {
+        public override string name { get; set; } = "lookLoot";
+        public override string displayName { get; set; } = "View loot";
+
+        public string targetName { get; set; }
+
+        public List<ItemStack> items;
+
+        public override void Interact(ref Player interactor, ICommandContext Context)
+        {
+            StringBuilder builder = new StringBuilder($"{Context.User.Username}: You look in the {targetName} and see:\n");
+            int i = 1;
+            foreach (ItemStack item in items)
+            {
+                builder.Append($"{i}: {item.ToStringDisplay()}\n");
+                i++;
+            }
+            Context.Channel.SendMessage(builder.ToString());
+        }
+    }
+}
