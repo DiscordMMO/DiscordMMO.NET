c7bff2e1a00ce8f6dabc7b0226f17585480aa5eb
TheMagzuz <markus.dam123@gmail.com>
2017-12-07 21:24:20 +0100

    Add a loot interaction

---
+++ DiscordMMO/Datatypes/Interactions/InteractionLoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using DiscordMMO.Util;
+
+namespace DiscordMMO.Datatypes.Interactions
+{
+    public class InteractionLoot : Interaction
+    {
+        public override string name { get; set; } = "loot";
+        public override string displayName { get; set; } = "Loot";
+
+        public List<ItemStack> items = new List<ItemStack>();
+
+        public override void Interact(ref Player interactor, ICommandContext Context)
+        {
+            StringBuilder b = new StringBuilder($"{Context.User.Username}: Looted:\n");
+            foreach (ItemStack item in items)
+            {
+                if (interactor.inventory.CanAdd(item))
+                {
+                    interactor.inventory.AddItem(item);
+                    b.Append(item.ToStringDisplay() + "\n");
+                }
+                else
+                {
+                    Context.Channel.SendMessage($"{Context.User.Username}: You do not have enough inventory space to loot that");
+                    break;
+                }
+            }
+            Context.Channel.SendMessage(b.ToString());
+        }
+    }
+}
