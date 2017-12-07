6650aff4a2dcec42c05e3b58ec50896afb91d368
TheMagzuz <markus.dam123@gmail.com>
2017-12-07 21:25:05 +0100

    Add gravestones

---
+++ DiscordMMO/Datatypes/Entities/EntityGravestone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordMMO.Datatypes.Interactions;
+
+namespace DiscordMMO.Datatypes.Entities
+{
+    public class EntityGravestone : Entity
+    {
+        public override string name { get; set; } = "gravestone";
+        public override string displayName { get; set; } = "\'s gravestone";
+
+        public string playerName { get; set; }
+        public List<ItemStack> items { get; set; }
+
+        public void FieldsInitialized()
+        {
+            displayName = $"{playerName}\'s gravestone";
+            interactions.Add(new InteractionLookLoot { targetName = displayName, items = items });
+            interactions.Add(new InteractionLoot { items = items });
+        }
+
+    }
+}
