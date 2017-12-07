dc62260a81e1e1ec459fd88f0e96cb877f6dc031
TheMagzuz <markus.dam123@gmail.com>
2017-12-07 21:46:59 +0100

    Added a creation time to entities

--- DiscordMMO/Datatypes/Entities/Entity.cs
+++ DiscordMMO/Datatypes/Entities/Entity.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using DiscordMMO.Datatypes.Interactions;
 using DiscordMMO.Datatypes.Interactions.Interactibles;
 
 namespace DiscordMMO.Datatypes.Entities
 {
+    [XmlRoot]
     public abstract class Entity : Interactible
     {
+        [XmlElement]
+        public DateTime createdAt { get; set; } = DateTime.Now;
 
+        [XmlIgnore]
         public override List<Interaction> interactions { get; set; }
 
     }
