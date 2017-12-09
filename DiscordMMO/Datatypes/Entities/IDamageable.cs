fa5b6e11b6d40c68c2e378d8f10fb36698cad195
TheMagzuz <markus.dam123@gmail.com>
2017-12-09 18:02:38 +0100

    Add a warning restore

--- DiscordMMO/Datatypes/Entities/IDamageable.cs
+++ DiscordMMO/Datatypes/Entities/IDamageable.cs
@@ -226,6 +226,7 @@ namespace DiscordMMO.Datatypes.Entities
             {
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 target.Die(attacker);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 return true;
             }
 
