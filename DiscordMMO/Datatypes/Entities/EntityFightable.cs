DiscordMMO/DiscordMMO.NET

commit 7f1d2deb19b4d62eba78bc2fab575548a4c668ef

Made the IDamageable events delegate, to allow them to be called from outside the class

DiscordMMO/Datatypes/Entities/EntityFightable.cs
@@ -57,26 +57,18 @@ namespace DiscordMMO.Datatypes.Entities
         [XmlIgnore]
         public abstract List<ItemStack> drops { get; }
 
-        public virtual event OnBeforeAttacked BeforeAttackedEvent;
-        public virtual event OnBeforeAttacking BeforeAttackingEvent;
+        public virtual OnBeforeAttacked BeforeAttackedEvent { get; set; }
+        public virtual OnBeforeAttacking BeforeAttackingEvent { get; set; }
 
-        public virtual event OnAfterAttacked AfterAttackedEvent;
-        public virtual event OnAfterAttacking AfterAttackingEvent;
+        public virtual OnAfterAttacked AfterAttackedEvent { get; set; }
+        public virtual OnAfterAttacking AfterAttackingEvent { get; set; }
+
+        public virtual bool CanAttack(ref OnAttackEventArgs args) => true;
 
         public EntityFightable()
         {
             health = maxHealth;
         }
-
-        public virtual void CallBeforeAttackedEvent(ref OnAttackEventArgs args, bool forced) => BeforeAttackedEvent?.Invoke(ref args, forced);
-
-        public virtual void CallBeforeAttackingEvent(ref OnAttackEventArgs args, bool forced) => BeforeAttackingEvent?.Invoke(ref args, forced);
-
-        public virtual void CallAfterAttackedEvent(ref OnAttackEventArgs args, bool forced) => AfterAttackedEvent?.Invoke(ref args, forced);
-
-        public virtual void CallAfterAttackingEvent(ref OnAttackEventArgs args, bool forced) => AfterAttackingEvent?.Invoke(ref args, forced);
-
-        public virtual bool CanAttack(ref OnAttackEventArgs args) => true;
 
         public abstract void OnOpponentDied(List<ItemStack> drops);
 

DiscordMMO/Datatypes/Entities/IDamageable.cs
@@ -14,11 +14,11 @@ namespace DiscordMMO.Datatypes.Entities
     public interface IDamageable
     {
 
-        event OnBeforeAttacked BeforeAttackedEvent;
-        event OnBeforeAttacking BeforeAttackingEvent;
+        OnBeforeAttacked BeforeAttackedEvent { get; set; }
+        OnBeforeAttacking BeforeAttackingEvent { get; set; }
 
-        event OnAfterAttacked AfterAttackedEvent;
-        event OnAfterAttacking AfterAttackingEvent;
+        OnAfterAttacked AfterAttackedEvent { get; set; }
+        OnAfterAttacking AfterAttackingEvent { get; set; }
 
         string name { get; }
 
@@ -45,6 +45,8 @@ namespace DiscordMMO.Datatypes.Entities
 
         void OnOpponentDied(List<ItemStack> drops);
 
+        #region Deprecated
+        /*
         /// <summary>
         /// This is a hack, to be able to call the <see cref="BeforeAttackedEvent"/> from the extension methods
         /// </summary>
@@ -64,8 +66,46 @@ namespace DiscordMMO.Datatypes.Entities
         /// This is a hack, to be able to call the <see cref="AfterAttackingEvent"/> from the extension methods
         /// </summary>
         void CallAfterAttackingEvent(ref OnAttackEventArgs args, bool forced);
-
+        */
+        #endregion
     }
+
+    public class DamageSourceUnknown : IDamageable
+    {
+        public string name => "unknown_source";
+
+        public int maxHealth => 0;
+
+        public int health { get; set; } = 0;
+
+        public int defence => 0;
+
+        public int attackDamage => 999999999;
+
+        public int accuracy => 999;
+
+        public int ticksUntilNextAttack { get; set; } = 0;
+
+        public List<ItemStack> drops => null;
+
+        public int attackRate => 1;
+
+        public OnBeforeAttacked BeforeAttackedEvent { get; set; }
+        public OnBeforeAttacking BeforeAttackingEvent { get; set; }
+        public OnAfterAttacked AfterAttackedEvent { get; set; }
+        public OnAfterAttacking AfterAttackingEvent { get; set; }
+
+        public bool CanAttack(ref OnAttackEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnOpponentDied(List<ItemStack> drops)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 
     public static class IDamageableHelper
     {
@@ -202,8 +242,8 @@ namespace DiscordMMO.Datatypes.Entities
             if (!attacker.CanAttack(ref args))
                 return false;
 
-            attacker.CallBeforeAttackingEvent(ref args, !args.triggersEffect);
-            target.CallBeforeAttackedEvent(ref args, !args.triggersEffect);
+            attacker.BeforeAttackingEvent(ref args, !args.triggersEffect);
+            target.BeforeAttackedEvent(ref args, !args.triggersEffect);
 
             if (args.cancelled)
                 return false;
@@ -218,8 +258,8 @@ namespace DiscordMMO.Datatypes.Entities
 
             if (args.triggersEffect)
             {
-                attacker.CallAfterAttackingEvent(ref args, !args.triggersEffect);
-                attacker.CallAfterAttackedEvent(ref args, !args.triggersEffect);
+                attacker.AfterAttackingEvent(ref args, !args.triggersEffect);
+                attacker.AfterAttackedEvent(ref args, !args.triggersEffect);
             }
 
             if (target.health <= 0)
@@ -236,6 +276,22 @@ namespace DiscordMMO.Datatypes.Entities
         }
 
         #endregion
+
+        /// <summary>
+        /// Damage <paramref name="target"/> by <paramref name="amount"/>, ignoring all armor, defence, etc.
+        /// </summary>
+        /// <param name="target">The target to damage</param>
+        /// <param name="amount">The amount to damage the target by</param>
+        public static void TrueDamage(this IDamageable target, int amount, IDamageable source)
+        {
+            target.health -= amount;
+            if (target.health <= 0)
+            {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                target.Die(source);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            }
+        }
 
 #endregion
 

DiscordMMO/Datatypes/Actions/ActionFighting.cs
@@ -56,7 +56,7 @@ namespace DiscordMMO.Datatypes.Actions
                 OnAttackEventArgs args = new OnAttackEventArgs(fighting, performer);
                 if (fighting.Attack(performer))
                 {
-                    // TODO: Probably some death handling
+                    performer.Die(fighting);
                     await Finish();
                     return;
                 }

DiscordMMO/Datatypes/Player.cs
@@ -169,11 +169,11 @@ namespace DiscordMMO.Datatypes
 
         #region IDamageable
 
-        public event OnBeforeAttacked BeforeAttackedEvent;
-        public event OnBeforeAttacking BeforeAttackingEvent;
+        public OnBeforeAttacked BeforeAttackedEvent { get; set; }
+        public OnBeforeAttacking BeforeAttackingEvent { get; set; }
 
-        public event OnAfterAttacked AfterAttackedEvent;
-        public event OnAfterAttacking AfterAttackingEvent;
+        public OnAfterAttacked AfterAttackedEvent { get; set; }
+        public OnAfterAttacking AfterAttackingEvent { get; set; }
 
         public int maxHealth => 30;
 
@@ -453,18 +453,11 @@ namespace DiscordMMO.Datatypes
             }
         }
 
-        public void CallBeforeAttackedEvent(ref OnAttackEventArgs args, bool forced) => BeforeAttackedEvent?.Invoke(ref args, forced);
-
-        public void CallBeforeAttackingEvent(ref OnAttackEventArgs args, bool forced) => BeforeAttackingEvent?.Invoke(ref args, forced);
-
-        public void CallAfterAttackedEvent(ref OnAttackEventArgs args, bool forced) => AfterAttackedEvent?.Invoke(ref args, forced);
-
-        public void CallAfterAttackingEvent(ref OnAttackEventArgs args, bool forced) => AfterAttackingEvent?.Invoke(ref args, forced);
-
         public void Die(IDamageable killer)
         {
-        
-            // TODO: Handle death
+
+            currentArea.CreateGravestone(this);
+            inventory.Clear();
             
             var pm = GetPrivateChannel().GetAwaiter().GetResult();
 

commit 358edbfafe8d9abd54f6a999db92e61dc2b281c3

Fixed serialization for entities, which was broken by commit 7f1d2deb

DiscordMMO/Datatypes/Entities/EntityFightable.cs
@@ -57,10 +57,14 @@ namespace DiscordMMO.Datatypes.Entities
         [XmlIgnore]
         public abstract List<ItemStack> drops { get; }
 
+        [XmlIgnore]
         public virtual OnBeforeAttacked BeforeAttackedEvent { get; set; }
+        [XmlIgnore]
         public virtual OnBeforeAttacking BeforeAttackingEvent { get; set; }
 
+        [XmlIgnore]
         public virtual OnAfterAttacked AfterAttackedEvent { get; set; }
+        [XmlIgnore]
         public virtual OnAfterAttacking AfterAttackingEvent { get; set; }
 
         public virtual bool CanAttack(ref OnAttackEventArgs args) => true;

DiscordMMO/Datatypes/Entities/EntityGoblin.cs
@@ -7,6 +7,7 @@ using DiscordMMO.Datatypes.Interactions;
 using DiscordMMO.Util;
 using DiscordMMO.Handlers;
 using System.Runtime.Serialization;
+using System.Xml.Serialization;
 
 namespace DiscordMMO.Datatypes.Entities
 {
@@ -36,11 +37,15 @@ namespace DiscordMMO.Datatypes.Entities
 
         public override List<Interaction> interactions { get; set; }
 
-        public override OnBeforeAttacked BeforeAttackedEvent { get; set; }
-        public override OnBeforeAttacking BeforeAttackingEvent { get; set; }
+        //[XmlIgnore]
+        //public override OnBeforeAttacked BeforeAttackedEvent { get; set; }
+        //[XmlIgnore]
+        //public override OnBeforeAttacking BeforeAttackingEvent { get; set; }
 
-        public override OnAfterAttacked AfterAttackedEvent { get; set; }
-        public override OnAfterAttacking AfterAttackingEvent { get; set; }
+        //[XmlIgnore]
+        //public override OnAfterAttacked AfterAttackedEvent { get; set; }
+        //[XmlIgnore]
+        //public override OnAfterAttacking AfterAttackingEvent { get; set; }
 
         public override List<ItemStack> drops
         {

DiscordMMO/Datatypes/Entities/IDamageable.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DiscordMMO.Util;
+using System.Xml.Serialization;
 
 namespace DiscordMMO.Datatypes.Entities
 {
@@ -13,11 +14,14 @@ namespace DiscordMMO.Datatypes.Entities
 
     public interface IDamageable
     {
-
+        [XmlIgnore]
         OnBeforeAttacked BeforeAttackedEvent { get; set; }
+        [XmlIgnore]
         OnBeforeAttacking BeforeAttackingEvent { get; set; }
 
+        [XmlIgnore]
         OnAfterAttacked AfterAttackedEvent { get; set; }
+        [XmlIgnore]
         OnAfterAttacking AfterAttackingEvent { get; set; }
 
         string name { get; }
@@ -213,6 +217,10 @@ namespace DiscordMMO.Datatypes.Entities
         public static async Task Die(this IDamageable damageable, IDamageable killer)
         {
             killer.OnOpponentDied(damageable.drops);
+            Player p = damageable as Player;
+
+            await p?.Die(killer);
+
         }
 
         #region Attacking
@@ -266,9 +274,7 @@ namespace DiscordMMO.Datatypes.Entities
 
             if (target.health <= 0)
             {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                target.Die(attacker);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                target.Die(attacker).GetAwaiter().GetResult();
                 return true;
             }
 
@@ -289,9 +295,7 @@ namespace DiscordMMO.Datatypes.Entities
             target.health -= amount;
             if (target.health <= 0)
             {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                target.Die(source);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                target.Die(source).GetAwaiter().GetResult();
             }
         }
 