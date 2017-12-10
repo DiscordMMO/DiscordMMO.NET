75988ed46b488c06b116d07251de9c1d01af5eba
TheMagzuz <markus.dam123@gmail.com>
2017-12-10 10:54:30 +0100

    Add a string.CapitalizeFirst extension method

--- DiscordMMO/Helpers/StringHelper.cs
+++ DiscordMMO/Helpers/StringHelper.cs
@@ -14,5 +14,18 @@ namespace DiscordMMO.Helpers
             return d.ToString(@"dd\:HH\:mm\:ss");
         }
 
+        // Stolen from https://stackoverflow.com/questions/4135317/make-first-letter-of-a-string-upper-case-with-maximum-performance/4135491#4135491
+        public static string CapitalizeFirst(this string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
+        }
+            
+
     }
 }
