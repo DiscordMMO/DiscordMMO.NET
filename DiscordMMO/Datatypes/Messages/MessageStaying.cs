0f4be163e8afb6f9e4ce451f92f56c9c7b79194e
TheMagzuz <markus.dam123@gmail.com>
2017-12-16 13:08:37 +0100

    Add a message that doesn't get deleted

---
+++ DiscordMMO/Datatypes/Messages/MessageStaying.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace DiscordMMO.Datatypes.Messages
+{
+    // TODO: Come up with a better name
+    public class MessageStaying : Message
+    {
+
+        public MessageStaying() : base()
+        {
+            expiry = DateTime.MaxValue.AddYears(-1);
+        }
+
+        public MessageStaying(IMessage msg) : this()
+        {
+            message = msg;
+        }
+
+    }
+}
