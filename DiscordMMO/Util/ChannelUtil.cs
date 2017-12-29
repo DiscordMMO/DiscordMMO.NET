f65457cdce3c2238a79a64b9f7934d04402cc2bb
TheMagzuz <markus.dam123@gmail.com>
2017-12-29 09:17:21 +0100

    Made the sync extensions for sending messages use the new message system

--- DiscordMMO/Util/ChannelUtil.cs
+++ DiscordMMO/Util/ChannelUtil.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
+using DiscordMMO.Handlers;
+using DiscordMMO.Datatypes.Messages;
 
 namespace DiscordMMO.Util
 {
     public static class ChannelUtil
     {
 
-        public static void SendMessage(this IMessageChannel channel, string msg)
+        public static Message SendMessage(this IMessageChannel channel, string msg, bool isTTS = false, Embed embed = null, RequestOptions options = null)
         {
-            channel.SendMessageAsync(msg).GetAwaiter().GetResult();
+            return SendMessage<MessageDefault>(channel, msg, isTTS, embed, options);
         }
 
+        public static T SendMessage<T>(this IMessageChannel channel, string msg, bool isTTS = false, Embed embed = null, RequestOptions options = null) where T : Message, new()
+        {
+            return MessageHandler.SendMessageAsync<T>(channel, msg, isTTS, embed, options).GetAwaiter().GetResult();
+        }
     }
 }
