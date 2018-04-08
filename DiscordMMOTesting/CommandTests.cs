72df74f39a2671304eb3b93be894c4e4f0d42b97
TheMagzuz <markus.dam123@gmail.com>
2018-04-08 09:02:27 +0200

    Start adding unit testing

---
+++ DiscordMMOTesting/CommandTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DiscordMMO;
+
+namespace DiscordMMOTesting
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void TestEquipCommand()
+        {
+            // Mark that we are running tests
+            Modules.unitTesting = true;
+
+            Program.Main();
+
+        }
+    }
+}
---
+++ DiscordMMOTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DiscordMMOTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiscordMMOTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4f11eff-4319-41c6-8cef-0c7b1b08e08e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
